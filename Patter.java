ba7caf4f4eebdc2cb6bc004a2a3bc1eb80bb09f2
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2021-02-06 10:14:32 +0530

    crt cod

--- Patter.java
+++ Patter.java
@@ -1,28 +1,46 @@
 import java.util.*;
 
 public class Patter {
+
+    public static int min(int i, int j) {
+        if (i < j)
+            return i;
+        else
+            return j;
+    }
+
     public static void main(String args[]) {
 
-        int n, row = 0, col;
+        int n, num = 1;
+
         Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number :");
         n = sc.nextInt();
+        System.out.println();
+
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            col = 1;
             for (int j = 0; j < n; j++) {
-                if (i == row && j == col) {
-                    System.out.print(n - i + " ");
-                    col += 1;
-
+                if (i == num || j == num) {
+                    arr[i][j] = n - min(i, j);
+                    // col += 1;
                 } else {
-                    System.out.print(n + " ");
+                    arr[i][j] = n;
                 }
 
             }
-            row += 1;
+            if (i >= 1)
+                num += 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                System.out.print(arr[i][j] + "  ");
+            }
             System.out.println();
         }
 
     }
-
 }
